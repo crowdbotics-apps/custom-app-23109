2e0c09b18621af5deab175c4475d699282850dc7
Crowdbotics <team@crowdbotics.com>
2020-12-02 18:41:17 +0000

    Update models

--- backend/storeapp/migrations/0001_initial.py
+++ backend/storeapp/migrations/0001_initial.py
@@ -7,15 +7,22 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Products',
+            name="Products",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('shoes', models.CharField(blank=True, max_length=256, null=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("shoes", models.CharField(blank=True, max_length=256, null=True)),
             ],
         ),
     ]
--- backend/storeapp/models.py
+++ backend/storeapp/models.py
@@ -5,10 +5,26 @@ from django.db import models
 class Products(models.Model):
     "Generated Model"
     shoes = models.CharField(
-        max_length=256,
         null=True,
         blank=True,
+        max_length=256,
     )
+    sneakers = models.ForeignKey(
+        "storeapp.Product",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="products_sneakers",
+    )
+
+
+class Product(models.Model):
+    "Generated Model"
+    name = models.CharField(
+        max_length=256,
+    )
+    price = models.FloatField()
+    image = models.URLField()
 
 
 # Create your models here.
